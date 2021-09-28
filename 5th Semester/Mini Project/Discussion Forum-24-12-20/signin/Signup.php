f891c55e98949933703f6b8b132e8e263b25ea29
Aman kumar <60618495+Amankumar2310@users.noreply.github.com>
2021-09-28 19:34:16 +0530

    Add files via upload

---
+++ 5th Semester/Mini Project/Discussion Forum-24-12-20/signin/Signup.php
@@ -0,0 +1,40 @@
+<?php  
+
+$servername ="localhost";
+$username = "root";
+$password = "";
+$dbname = "signup";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+	die("connection failed");
+}
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$salt = "Akarshit";
+$password_encrypted = sha1($password.$salt);
+
+$sql= "INSERT INTO registration(name,email, password) 
+VALUES ('$name', '$email', '$password_encrypted')";
+
+
+if($conn->query($sql) === TRUE){
+	?>
+	<script>
+		alert('Values have been inserted');
+	</script>
+	<?php
+}
+else{
+	?>
+	<script>
+		alert('Values did not insert');
+	</script>
+	<?php
+}
+
+
+?>
